2342fd78edb4e81ee724d4be3ce152da47122641
Piyush <piyushkachariya111@gmail.com>
2015-10-18 23:11:41 -0700

    Initial Commit

---
+++ KidsPuzzle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KidsPuzzle
+//
+//  Created by Piyush Kachariya on 10/18/15.
+//  Copyright © 2015 Kachariya&co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KidsPuzzle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KidsPuzzle
+//
+//  Created by Piyush Kachariya on 10/18/15.
+//  Copyright © 2015 Kachariya&co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
